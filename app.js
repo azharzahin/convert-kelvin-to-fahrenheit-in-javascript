674462afb5ca37b5f2f84c3506082dccf61e528d
azharzahin <18713606+azharzahin@users.noreply.github.com>
2020-04-29 09:38:24 +0530

    Create app.js
    convert kelvin to fahrenheit

---
+++ app.js
@@ -0,0 +1,21 @@
+// date is 29th april 2020 time is 9:18am doing this project after completing all basics part of js today from 6am to now...!!!
+
+// this is today's forecast in kalvin
+const kelvin = 311;
+
+// now converting kelvin to celsius using new variable and stored value in celcius
+
+const celsius = 293 - 273 ; 
+
+// now converting Celsius to Fahrenheits using this formula and store the value in fahrenheit
+
+// using math.floor to got a exact value of fahrenheit no decimal value please
+
+ const fahrenheit = Math.floor(celsius * (9/5) + 32) ; 
+ 
+ //now printing value in fahrenheit....!!!
+
+console.log (`The temperature is ${fahrenheit} degrees Fahrenheit. `);
+
+
+//using this app we can convert any kelvin value to fahrenheit...
